1b37ad88b53793c5d3c46548facaab6c3e0bdb3e
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-10 15:29:27 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//  Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Random
+let random = Math.ceil(Math.random() * 10);
+
+// Challenge 3: Modulo
+let num3 = 24;
+let num4 = 20;
+let mod = num3 % num4;
+
+// Max
+let max = Math.max(5, 10, 15, 20);
